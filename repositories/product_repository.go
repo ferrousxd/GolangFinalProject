d120d486134ad97384e8014779feb995a1b98c5c
OsetinskiyPirog <ferrousxd@icloud.com>
2020-11-07 18:37:10 +0600

    Added User model and repositories

---
+++ repositories/product_repository.go
@@ -0,0 +1,22 @@
+package repositories
+
+import (
+	"GolangFinalProject/models"
+	"database/sql"
+)
+
+type ProductRepository struct {
+	connection *sql.DB
+}
+
+func (userRepo *UserRepository) InsertProduct(product models.Product) {
+
+}
+
+func (userRepo *UserRepository) DeleteProduct(productId int) {
+
+}
+
+func (userRepo *UserRepository) GetAllProducts() []*models.Product {
+	return nil
+}
